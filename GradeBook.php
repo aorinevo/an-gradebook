1788286e3aea38e572fd94c654d4f7cec5fbdcb3
anevo <anevo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-14 17:18:20 +0000

    switched tag from 1.2 to 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/an-gradebook/trunk@787716 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- GradeBook.php
+++ GradeBook.php
@@ -3,7 +3,7 @@
 Plugin Name: GradeBook
 Plugin URI: http://www.aorinevo.com/
 Description: A simple GradeBook plugin
-Version: 1.1
+Version: 1.2.1
 Author: Aori Nevo
 Author URI: http://www.aorinevo.com
 License: GPL
