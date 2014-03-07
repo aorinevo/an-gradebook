1c1fefa34e85de8f96be5451463a98a0dadd2674
anevo <anevo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-07 22:42:28 +0000

    stagging v2.0.2
    git-svn-id: https://plugins.svn.wordpress.org/an-gradebook/trunk@871489 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- GradeBook.php
+++ GradeBook.php
@@ -3,7 +3,7 @@
 Plugin Name: GradeBook
 Plugin URI: http://www.aorinevo.com/
 Description: A simple GradeBook plugin
-Version: 2.0.1
+Version: 2.0.2
 Author: Aori Nevo
 Author URI: http://www.aorinevo.com
 License: GPL
