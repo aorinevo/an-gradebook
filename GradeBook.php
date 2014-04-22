a27032697ed2df3d47965add796af2efe7eb5fd5
Aori Matt Nevo <aorinevo@gmail.com>
2014-04-22 18:07:45 -0400

    new feature DeleteStudent modal

--- GradeBook.php
+++ GradeBook.php
@@ -3,7 +3,7 @@
 Plugin Name: GradeBook
 Plugin URI: http://www.aorinevo.com/
 Description: A simple GradeBook plugin
-Version: 2.3.1
+Version: 2.3.2
 Author: Aori Nevo
 Author URI: http://www.aorinevo.com
 License: GPL
@@ -704,7 +704,51 @@ ob_start();
        		</div>
     	</div>  
     </div> 
-    </script>    
+    </script>   
+    
+    <script id="delete-student-template" type="text/template">
+    <div id="delete-student-form-container" class="media-modal wp-core-ui"> 
+    <a class="media-modal-close" title="Close"><span class="media-modal-icon"></span></a>
+    	<div class="media-modal-content">
+    	    <div class="media-frame wp-core-ui">
+				<div class="media-frame-menu">
+					<div class="media-menu">
+						<a href="#" class="media-menu-item">Delete Student</a>
+						<div class="separator"></div>
+					</div>
+				</div>    	    
+    	    	<div class="media-frame-title">
+    				<h1>Delete Student</h1>
+    			</div>    
+    	    	<div class="media-frame-content">
+    				<form id="edit-student-form">      
+         				<input type="hidden" name="action" value="delete_student"/>
+				        <input type="hidden" name="id" value="<%= student ? student.get('id') : '' %>"/> 
+				        Delete <%= student.get('firstname')%> <%= student.get('lastname')%> with student id <%= student.get('id')%> from:  
+				        <br/>      
+						<select name="delete_options">
+							<option value="gradebook">this gradebook only.</option>
+							<option value="all_gradebooks">all gradebooks.</option>
+							<option value="database">the wordpress database.</option>
+						</select>
+						<br/>
+						Removing a student from the wordpress database will also remove that student from all gradebooks.
+				        <p/>
+				        <input type="hidden" name="gbid" value="<%= gradebook.get('id') %>"/>
+    				</form>
+    			</div>			
+        		<div class="media-frame-toolbar">
+    				<div class="media-toolbar">         
+     					<div class="media-toolbar-secondary"></div>
+     					<div class="media-toolbar-primary">
+     						<button id="delete-student-delete" class="button media-button button-primary button-large">Delete</button>
+     					</div>
+       				</div>
+       			</div> 
+       		</div>
+    	</div>  
+    </div> 
+    </script>      
     
     <script id="edit-assignment-template" type="text/template">
     <div id="edit-assignment-form-container" class="media-modal wp-core-ui"> 
