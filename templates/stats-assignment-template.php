a6752936cdabf20cc710ac4e7e33e5a1a9ae9b48
anevo <anevo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-26 01:58:24 +0000

    Version 2.3.5:
    * Added an Assignment Statistics modal. Instead of the assignment pie chart being displayed once an assignment column is selected, you now click the Assignment Statistics button and a modal appears containing the pie chart.  This will allow for easy addition of other charts in the future.
    
    git-svn-id: https://plugins.svn.wordpress.org/an-gradebook/tags/2.3.5@902955 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/stats-assignment-template.php
@@ -0,0 +1,37 @@
+<?php
+ob_start();
+?>
+    <script id="stats-assignment-template" type="text/template">
+    <div id="stats-assignment-form-container" class="media-modal wp-core-ui"> 
+    <a class="media-modal-close" title="Close"><span class="media-modal-icon"></span></a>
+    	<div class="media-modal-content">
+    	    <div class="media-frame wp-core-ui">
+				<div class="media-frame-menu">
+					<div class="media-menu">
+						<a href="#" class="media-menu-item">Assignment Statistics</a>
+						<div class="separator"></div>
+					</div>
+				</div>
+    	    	<div class="media-frame-title">
+    				<h1>Assignment Statistics</h1>
+    			</div>    
+    	    	<div class="media-frame-content">
+					<div id = "chart_div"></div>
+    			</div>    			
+        		<div class="media-frame-toolbar">
+    				<div class="media-toolbar">         
+     					<div class="media-toolbar-secondary"></div>
+     					<div class="media-toolbar-primary">
+							<button id="stats-assignment-close" class="button media-button button-primary button-large">Close</button>     					
+     					</div>
+       				</div>
+       			</div> 
+       		</div>
+    	</div>  
+    </div> 
+    </script>     
+<?php
+$mytemplate = ob_get_contents();
+ob_get_clean();
+echo $mytemplate;
+?>
