ab236651f923a9e88d573bc00d812ecf5196bf4a
anevo <anevo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-30 22:28:31 +0000

    Added a functions.php file to hold all commonly used functions (i.e. hooks, sorting, etc...) Added a delete_user hook so that when an admin deletes a user from the database through the Users admin page, the hook deletes the user from any gradebook table that they are associated with.
    git-svn-id: https://plugins.svn.wordpress.org/an-gradebook/tags/2.3.7@905960 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/gradebook-interface-template.php
@@ -0,0 +1,29 @@
+<?php
+ob_start();
+?>
+    <script id="gradebook-interface-template" type="text/template">
+    <hr/>
+    <div id="gradebook-interface-buttons-container">
+    <button type="button" id="add-student" class="wp-core-ui button">Add Student</button>
+    <button type="button" id="edit-student" class="wp-core-ui button">Edit Student</button>  
+    <button type="button" id="delete-student" class="wp-core-ui button">Delete Student</button>
+    <button type="button" id="add-assignment" class="wp-core-ui button">Add Assignment</button>
+    <button type="button" id="edit-assignment" class="wp-core-ui button">Edit Assignment</button>  
+    <button type="button" id="stats-assignment" class="wp-core-ui button">Assignment Statistics</button>      
+    <button type="button" id="delete-assignment" class="wp-core-ui button">Delete Assignment</button>
+    </div>    
+    <hr/>
+    <table id="an-gradebook-container" class="wp-list-table widefat fixed pages">  
+    <thead id="students-header">
+      <tr>
+        <th><div><span>First Name</span> <span class="sorting-indicator"></span> </div></th><th>Last Name</th><th>ID</th>
+      </tr>
+    </thead>
+    <tbody id="students"></tbody>
+    </table>
+    </script>
+<?php
+$mytemplate = ob_get_contents();
+ob_get_clean();
+echo $mytemplate;
+?>
