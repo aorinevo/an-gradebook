641af4df736b412cdcbf3355b9724683f5c97e99
Aori Matt Nevo <aorinevo@gmail.com>
2014-04-30 18:20:51 -0400

    Added a functions.php file to hold all commonly used functions (i.e. hooks, sorting, etc...) Added a delete_user hook so that when an admin deletes a user from the database through the Users admin page, the hook deletes the user from any gradebook table that they are associated with.

--- GradeBook.php
+++ GradeBook.php
@@ -57,27 +57,8 @@ class AN_GradeBook_Scripts{
 	} 	
 }
 
-//Create/Upgrade Database
 include_once( dirname( __FILE__ ) . '/an-gradebook-database.php' );
-
-	function build_sorter($key) {
-   		return function ($a, $b) use ($key) {
-        	return strnatcmp($a[$key], $b[$key]);
-	    };
-	}	
-	
-	function gradebook_check_user_role( $role, $user_id = null ) {
- 		if ( is_numeric( $user_id ) ){
-			$user = get_userdata( $user_id );
-		}
-    	else{
-        	$user = wp_get_current_user();
- 		}
-    	if ( empty( $user ) ){
-			return false;
-		}
- 		return in_array( $role, (array) $user->roles );
-	}
+include_once( dirname( __FILE__ ) . '/functions.php' );
 
 class AN_GradeBookAPI{
 	public function __construct(){
