b2cce1f3e55475f574851bddd03645633a65dbdf
Aori Nevo <aorinevo@gmail.com>
2014-11-14 00:21:56 -0500

    updated readme.txt

--- GradeBook.php
+++ GradeBook.php
@@ -3,7 +3,7 @@
 Plugin Name: GradeBook
 Plugin URI: http://www.aorinevo.com/
 Description: A simple GradeBook plugin
-Version: 2.4.7
+Version: 2.4.8
 Author: Aori Nevo
 Author URI: http://www.aorinevo.com
 License: GPL
