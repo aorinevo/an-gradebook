55d21c7fc71f441a71ea7b6ff4471c84d2f7da1c
Aori Nevo <aorinevo@gmail.com>
2015-10-27 22:32:32 -0400

    missing files

---
+++ js/build-an-gradebook-settings.js
@@ -0,0 +1,22 @@
+({
+    //By default load any module IDs from js/lib
+    baseUrl: '.',
+    paths : {
+    	'models' : 'app/models',
+    	'views' : 'app/views',
+    	'jquery' : 'lib/jquery',
+		'jquery-ui' : 'lib/jquery-ui',    	
+    	'backbone': 'lib/backbone',
+    	'underscore': 'lib/underscore',
+    	'bootstrap': 'lib/bootstrap',
+    	'bootstrap3-typeahead': 'lib/bootstrap3-typeahead/bootstrap3-typeahead.min',    	
+    	'goog': 'lib/goog',
+    	'async': 'lib/async',
+    	'propertyParser': 'lib/propertyParser'  	    	
+    },
+    name: 'an-gradebook-settings',
+    out: 'an-gradebook-settings-min.js',
+	shim: {
+		'bootstrap':['jquery']       
+    }
+})
