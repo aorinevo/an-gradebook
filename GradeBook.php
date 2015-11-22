cf1f2d6c08b1c60e61dafc4a860001c68420d16c
Aori Nevo <aorinevo@gmail.com>
2015-11-21 21:22:40 -0500

    more stagging for v4.0.5

--- GradeBook.php
+++ GradeBook.php
@@ -41,7 +41,7 @@ add_action( 'admin_menu', 'register_an_gradebook_menu_page' );
 	
 function enqueue_an_gradebook_scripts($hook){
 	if( $hook == "toplevel_page_an_gradebook" || $hook=='gradebook_page_an_gradebook_settings'){
-		$an_gradebook_develop = true;
+		$an_gradebook_develop = false;
 		$app_base = plugins_url('js',__FILE__);	
 		wp_register_style( 'jquery_ui_css', $app_base.'/lib/jquery-ui/jquery-ui.css', array(), null, false );	
 		wp_register_style( 'GradeBook_css', plugins_url('GradeBook.css',__File__), array('bootstrap_css','jquery_ui_css'), null, false );				
